42eaf4105ecf40d14b2d4f818e494a69ffcff642
komal-agrawal <komal.agrawal2468@gmail.com>
2019-04-15 17:43:05 -0700

    Change package name

--- JavaFXApplication1.java
+++ JavaFXApplication1.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxapplication1;
+package pkginterface.CSE360Project;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
